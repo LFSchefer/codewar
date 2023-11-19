ee537f3bfe9e0b78c6bb8b73e5a041b8af6371b9
louis frederic schefer <lf.schefer@gmail.com>
2023-11-19 18:20:50 +0100

    snake case

--- codewar.rb
+++ codewar.rb
@@ -1,20 +1,22 @@
-def duplicate_count(text)
-  number_of_duplicate = []
-   text.chars.each do |letter|
-    if text.downcase.chars.count(letter.downcase) > 1
-      number_of_duplicate << letter.downcase
+def to_underscore(string)
+  new_string = []
+   string.to_s.each_char do |char|
+    if char.upcase == char && char.to_i.to_s != char
+      new_string << "_"
     end
+    new_string << char.downcase
   end
-  p number_of_duplicate.uniq.size
+  if new_string[0] == "_"
+    new_string.delete_at(0)
+  end
+  p new_string.join()
 end
 
-duplicate_count("")
-p "expected 0"
-duplicate_count("abcde")
-p "expected 0"
-duplicate_count("abcdeaa")
-p "expected 1"
-duplicate_count("abcdeaB")
-p "expected 2"
-duplicate_count("Indivisibilities")
-p "expected 2"
+to_underscore("TestController")
+p  "EXPECTED: test_controller"
+to_underscore("MoviesAndBooks")
+p  "EXPECTED: movies_and_books"
+to_underscore("App7Test")
+p  "EXPECTED: app7_test"
+to_underscore(1)
+p  "EXPECTED: 1"
