30452230e0c3c39820aaaa4f109afff2ff876464
louis frederic schefer <lf.schefer@gmail.com>
2023-11-19 17:55:38 +0100

    duplicate count

--- codewar.rb
+++ codewar.rb
@@ -1,83 +1,20 @@
-def protein(rna)
-  #your code here
-  #you can copy and paste the codons from the description into a hash
-  #or use the preloaded $codons hash
-
-  code = {
-   # Phenylalanine
-    'UUC'=>'F', 'UUU'=>'F',
-    # Leucine
-    'UUA'=>'L', 'UUG'=>'L', 'CUU'=>'L', 'CUC'=>'L','CUA'=>'L','CUG'=>'L',
-    # Isoleucine
-    'AUU'=>'I', 'AUC'=>'I', 'AUA'=>'I',
-    # Methionine
-    'AUG'=>'M',
-    # Valine
-    'GUU'=>'V', 'GUC'=>'V', 'GUA'=>'V', 'GUG'=>'V',
-    # Serine
-    'UCU'=>'S', 'UCC'=>'S', 'UCA'=>'S', 'UCG'=>'S', 'AGU'=>'S', 'AGC'=>'S',
-    # Proline
-    'CCU'=>'P', 'CCC'=>'P', 'CCA'=>'P', 'CCG'=>'P',
-    # Threonine
-    'ACU'=>'T', 'ACC'=>'T', 'ACA'=>'T', 'ACG'=>'T',
-    # Alanine
-    'GCU'=>'A', 'GCC'=>'A', 'GCA'=>'A', 'GCG'=>'A',
-    # Tyrosine
-    'UAU'=>'Y', 'UAC'=>'Y',
-    # Histidine
-    'CAU'=>'H', 'CAC'=>'H',
-    # Glutamine
-    'CAA'=>'Q', 'CAG'=>'Q',
-    # Asparagine
-    'AAU'=>'N', 'AAC'=>'N',
-    # Lysine
-    'AAA'=>'K', 'AAG'=>'K',
-    # Aspartic Acid
-    'GAU'=>'D', 'GAC'=>'D',
-    # Glutamic Acid
-    'GAA'=>'E', 'GAG'=>'E',
-    # Cystine
-    'UGU'=>'C', 'UGC'=>'C',
-    # Tryptophan
-    'UGG'=>'W',
-    # Arginine
-    'CGU'=>'R', 'CGC'=>'R', 'CGA'=>'R', 'CGG'=>'R', 'AGA'=>'R', 'AGG'=>'R',
-    # Glycine
-    'GGU'=>'G',  'GGC'=>'G', 'GGA'=>'G', 'GGG'=>'G',
-    # Stop codon
-    'UAA'=>'Stop', 'UGA'=>'Stop', 'UAG'=>'Stop'
-    }
-
-    sec = rna.chars.each_slice(3).map(&:join)
-    reponse = []
-    sec.each do |x|
-      reponse << code[x]
-      break if code[x] == "Stop"
+def duplicate_count(text)
+  number_of_duplicate = []
+   text.chars.each do |letter|
+    if text.downcase.chars.count(letter.downcase) > 1
+      number_of_duplicate << letter.downcase
     end
-
-     reponse.delete("Stop")
-     return reponse.join()
-
+  end
+  p number_of_duplicate.uniq.size
 end
 
-protein('AUG')
-p 'M'
-protein('AUGUGA')
-p 'M'
-protein('AUGGUUAGUUGA')
-p 'MVS'
-protein('UGCGAUGAAUGGGCUCGCUCC')
-p 'CDEWARS'
-protein('AUGUCCUUCCAUCAAGGAAACCAUGCGCGUUCAGCUUUCUGA')
-p 'MSFHQGNHARSAF'
-protein('AUGCUUCAAGUGCACUGGAAAAGGAGAGGGAAAACCAGUUGA')
-p 'MLQVHWKRRGKTS'
-protein('AUGGCGUUCAGCUUUCUAUGGAGGGUAGUGUACCCAUGCUGA')
-p 'MAFSFLWRVVYPC'
-protein('AUGCAGCUUUCUAUGGAGGGUAGUGUUAACUACCACGCCUGA')
-p 'MQLSMEGSVNYHA'
-protein('AUGCUAUGGAGGGUAGUGUUAACUACCACGCCCAGUACUUGA')
-p 'MLWRVVLTTTPST'
-protein('AUGUAUCCUUCCAUCAAGGAAACCAUGCGCGUUCAGCUUUCUAUGGAGGGUAGUGUUAACUACCACGCCUUCAAGUGCACUGGAAAAGGAGAGGGAAAACCAUACGAAGGCACCCAAAGCCUGAAUAUUACAAUAACUGAAGGAGGUCCUCUGCCAUUUGCUUUUGACAUUCUGUCACACGCCUUUCAGUAUGGCAUCAAGGUCUUCGCCAAGUACCCCAAAGAAAUUCCUGACUUCUUUAAGCAGUCUCUACCUGGUGGUUUUUCUUGGGAAAGAGUAAGCACCUAUGAAGAUGGAGGAGUGCUUUCAGCUACCCAAGAAACAAGUUUGCAGGGUGAUUGCAUCAUCUGCAAAGUUAAAGUCCUUGGCACCAAUUUUCCCGCAAACGGUCCAGUGAUGCAAAAGAAGACCUGUGGAUGGGAGCCAUCAACUGAAACAGUCAUCCCACGAGAUGGUGGACUUCUGCUUCGCGAUACCCCCGCACUUAUGCUGGCUGACGGAGGUCAUCUUUCUUGCUUCAUGGAAACAACUUACAAGUCGAAGAAAGAGGUAAAGCUUCCAGAACUUCACUUUCAUCAUUUGCGUAUGGAAAAGCUGAACAUAAGUGACGAUUGGAAGACCGUUGAGCAGCACGAGUCUGUGGUGGCUAGCUACUCCCAAGUGCCUUCGAAAUUAGGACAUAACUGA')
-
-p 'MYPSIKETMRVQLSMEGSVNYHAFKCTGKGEGKPYEGTQSLNITITEGGPLPFAFDILSHAFQYGIKVFAKYPKEIPDFFKQSLPGGFSWERVSTYEDGGVLSATQETSLQGDCIICKVKVLGTNFPANGPVMQKKTCGWEPSTETVIPRDGGLLLRDTPALMLADGGHLSCFMETTYKSKKEVKLPELHFHHLRMEKLNISDDWKTVEQHESVVASYSQVPSKLGHN'
+duplicate_count("")
+p "expected 0"
+duplicate_count("abcde")
+p "expected 0"
+duplicate_count("abcdeaa")
+p "expected 1"
+duplicate_count("abcdeaB")
+p "expected 2"
+duplicate_count("Indivisibilities")
+p "expected 2"
