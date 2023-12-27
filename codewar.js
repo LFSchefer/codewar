23be7562e75bab4412131eaa7f7eafd4413ce984
louis frederic schefer <lf.schefer@gmail.com>
2023-12-27 21:29:52 +0100

    function

--- codewar.js
+++ codewar.js
@@ -1,16 +1,5 @@
-function generateLink(user) {
-  const userLink = encodeURIComponent(user);
-  return `http://www.codewars.com/users/${userLink}`
-}
-
-
-generateLink('matt c')
-console.log(' Expected :http://www.codewars.com/users/matt%20c');
-generateLink('g964')
-console.log(' Expected :http://www.codewars.com/users/g964');
-generateLink('GiacomoSorbi')
-console.log(' Expected :http://www.codewars.com/users/GiacomoSorbi');
-generateLink('ZozoFouchtra')
-console.log(' Expected :http://www.codewars.com/users/ZozoFouchtra');
-generateLink('colbydauph')
-console.log(' Expected :http://www.codewars.com/users/colbydauph');
+function add(n) {
+  return function addN(x) {
+    return x + n
+  };
+};
