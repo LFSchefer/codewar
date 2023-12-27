d54aff8e93c5819d89adc5b20121490042fe36f0
louis frederic schefer <lf.schefer@gmail.com>
2023-12-27 22:01:16 +0100

    average arguments

--- codewar.js
+++ codewar.js
@@ -1,5 +1,16 @@
-function add(n) {
-  return function addN(x) {
-    return x + n
-  };
-};
+var Calculator = {
+  average: function() {
+    let keys = Object.keys(this.average.arguments);
+    if (keys.length === 0) {
+      return 0
+    };
+    let sum = 0;
+   keys.forEach(key => {
+     sum += this.average.arguments[keys[key]]
+    })
+    return sum/keys.length
+  }
+ };
+
+ Calculator.average(3,4,9)
+ Calculator.average()
