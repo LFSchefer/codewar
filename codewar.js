4df718a69a2169c4c6840f83fc55038d6a25f1ca
louis frederic schefer <lf.schefer@gmail.com>
2024-02-13 07:24:05 +0100

    sort object for twich chat score

--- codewar.js
+++ codewar.js
@@ -3,15 +3,41 @@ const obj = {
   b: 24,
   c: 18,
   d: 4,
-  e: 34
+  e: 34,
+  x: 2
 }
 
-obj.x = 55;
+obj.x === undefined ? obj.x = 55 : obj.x += 55;
+
+// console.log(obj)
 
 const result = Object.entries(obj).sort((a,b)=>b[1]-a[1])
 
-console.log(result)
+// console.log(result)
 
 result.forEach(val => {
-  console.log(`${val[0]}: ${val[1]}`)
+  // console.log(`${val[0]}: ${val[1]}`)
 })
+
+const score = {
+  mel: {
+    color: "#00000",
+    score: 12},
+  sab: {
+    color: "#00000",
+    score: 21},
+  paul: {
+    color: "#00000",
+    score: 8},
+  nico: {
+    color: "#00000",
+    score: 42},
+  lf: {
+    color: "#fffff",
+    score: 2},
+}
+
+score.lf === undefined ? score.lf = {color: "#fffff", score: 7} : score.lf.score += 7
+
+const test = Object.entries(score).sort((a,b)=>{return b[1].score-a[1].score})
+console.log(test)
